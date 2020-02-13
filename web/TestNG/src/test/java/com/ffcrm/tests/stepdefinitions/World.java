442089d125ceb22f3dff811c7fbab76143e639fd
suresh3qilabs <suresh.babu@3qilabs.com>
2020-02-13 10:28:18 +0530

    nothing added

--- web/TestNG/src/test/java/com/ffcrm/tests/stepdefinitions/World.java
+++
@@ -1,9 +0,0 @@
-package com.ffcrm.tests.stepdefinitions;
-
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-
-public class World {
-//     System.setProperty("webdriver.chrome.driver", "/Users/sureshbabu/Desktop/Dev_Ruby/shamisen/bin/chromedriver");
-    public WebDriver driver = new ChromeDriver();
-}
