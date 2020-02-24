fb3434c8e2ad33760b98393541d7486b3f7ea224
suresh3qilabs <suresh.babu@3qilabs.com>
2020-02-24 14:15:58 +0530

    modified 2 files

--- web/cucumber/step_definitions/mystepsSD_dupl.rb
+++ web/cucumber/step_definitions/mystepsSD_dupl.rb
@@ -24,3 +24,20 @@ Then(/^I click the link with text "(.*?)"$/) do |text|
   @browser.link(:text, "#{text}").click
 end
 
+
+
+
+
+
+########################################
+Then(/^I enter text "(.*?)" in the search box$/) do |arg1|
+  @browser.text_field(:id,'gbqfq').set("#{arg1}")
+end
+
+Then(/^I click the search button$/) do
+  @browser.button(:id,'gbqfb').click
+end
+
+Then(/^I click the link with text "(.*?)"$/) do |text|
+  @browser.link(:text, "#{text}").click
+end
